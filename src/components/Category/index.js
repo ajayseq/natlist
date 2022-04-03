257f79c2e23ee70eabc59869c91afabb2908aca2
Ajay S <ajay@sicloot.com>
2022-04-03 00:09:48 -0400

    get data from API

---
+++ src/components/Category/index.js
@@ -0,0 +1,11 @@
+import {useContext} from 'react';
+import {DataContext} from '../../App';
+
+const Category = (props) => {
+  const dataContext = useContext(DataContext);
+
+  return <div>Hello</div>;
+
+}
+
+export default Category;
--- src/components/Categorylist/index.js
+++ src/components/Categorylist/index.js
@@ -1,26 +1,45 @@
-import {useEffect, useContext} from 'react';
+import {useState, useEffect, useContext} from 'react';
 import {DataContext} from '../../App';
+import Category from '../Category';
 
 const Categorylist = (props) => {
+  const [categoryData, setCategoryData] = useState('');
   const dataContext = useContext(DataContext);
+  let dataUrl;
 
+  //use categories for top-level categories else go to subcategories of the given taxon
+  if (props.taxonid == 1) {
+    dataUrl = `https://sicloot.com/private/lifelist/api/categories.php?locationid=`+dataContext.locationID;
+  } else {
+    dataUrl = `https://sicloot.com/private/lifelist/api/subcategories.php?locationid=`+dataContext.locationID+`taxonid=`+props.taxonid;
+  }
+
+  //make API call to retrieve categories or subcategories
   useEffect(() => {
-
-    let dataUrl = `https://sicloot.com/private/lifelist/api/categories.php?locationid=`+dataContext.locationID;
-
     const makeApiCall = () => {
       fetch(dataUrl)
       .then(res => res.json())
       .then(data =>  {
-        console.log('categoryData: ', data);
-        //setMovieData((data))
+        setCategoryData(data);
        });
     }
     makeApiCall()
 
   }, [])
 
-  return <div>Categories</div>;
+  // const categories = categoryData.map((taxon) =>
+  //     <Category key={taxon.taxonid}
+  //               scientificname={taxon.scientificname}
+  //               commonname={taxon.commonname}
+  //               obs={taxon.obs}
+  //               species={taxon.species}
+  //      />
+  //   );
+
+  return (
+    <div>
+    </div>
+  );
 
 }
 
