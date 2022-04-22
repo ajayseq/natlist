df2f53fc1488cdb52350024cf45726cabc746cb7
Ajay S <ajay@sicloot.com>
2022-04-21 22:45:58 -0400

    add iNaturalist API ID info to tooltip

--- src/components/Obs/index.js
+++ src/components/Obs/index.js
@@ -1,11 +1,15 @@
-import {useState, useContext} from 'react';
+import {useState, useContext, useEffect} from 'react';
 import {DataContext} from '../Home';
 import moment from 'moment';
 import { Tooltip, Button } from 'reactstrap';
+import parse from 'html-react-parser';
 
 const Obs = (props) => {
   const dataContext = useContext(DataContext);
   const [tooltipOpen, setTooltipOpen] = useState(false);
+  const [iNatData, setINatData] = useState();
+  let iNaturalistObsID = props.idlink.slice(props.idlink.lastIndexOf("/")+1, props.idlink.length);
+  let dataUrl = `https://api.inaturalist.org/v1/observations/` + iNaturalistObsID;
 
   const starttime = () => {
     return moment(props.starttime).format('YYYY-MM-DD h:mma');
@@ -34,21 +38,64 @@ const Obs = (props) => {
           );
   });
 
+  useEffect(() => {
+    const makeApiCall = () => {
+      fetch(dataUrl)
+      .then(res => res.json())
+      .then(data =>  {
+        setINatData(data);
+        console.log('iNaturalist API URL: ', dataUrl);
+        console.log('iNaturalist data: ', data.results[0].identifications);
+       });
+    }
+    if (props.idlink.length > 1) {
+      makeApiCall();
+    }
+  }, [])
+
+  const iNaturalistIDs = (data) => {
+    let idText = '';
+
+    if (data) {
+      idText = '<table>';
+      data.results[0].identifications.forEach(id => {
+        if (JSON.parse(id.current)) {
+          idText += '<tr><td valign="top">' + id.user.login + ':';
+          if ((id.user.name.length > 0) && (id.user.name !== 'sicloot')) {
+            idText += '<br />(' + id.user.name + ')';
+          }
+          idText += '</td><td valign="top">&nbsp;&nbsp;<i>' + id.taxon.name + '</i>';
+          if (id.taxon.preferred_common_name) {
+            idText += ' - ' + id.taxon.preferred_common_name;
+          }
+          if (id.body) {
+            idText += '<br />&nbsp;&nbsp;"' + id.body + '"';
+          }
+          idText += '</td></tr>';
+        }
+      });
+      idText += '</table>';
+    }
+    console.log('idText: ', idText);
+    return idText;
+  }
+
   //if no ID link, do not provide link
   const calcIDLink = () => {
     if (props.idlink.length < 1) {
       return '';
     } else {
-      return (
+    return (
           <>
            <span>&nbsp;&nbsp;&nbsp;</span>
-             <Button id="IDToolTip" color="link"><a href={props.idlink}>ID Link</a></Button>
+             <Button id={'IDToolTip' + iNaturalistObsID} color="link"><a href={props.idlink}>ID Link</a></Button>
                          <Tooltip
+                             style={{maxWidth: '700px', textAlign: 'left', color: 'orange'}}
                              isOpen={tooltipOpen}
                              placement="right"
-                             target="IDToolTip"
+                             target={'IDToolTip' + iNaturalistObsID}
                              toggle={() => { setTooltipOpen(!tooltipOpen) }}>
-                             Sample Tooltip Text...
+                             {parse(iNaturalistIDs(iNatData))}
                          </Tooltip>
 
           </>
