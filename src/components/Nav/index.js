f6b02305242b3b3e6b745923ef3b7d203315f344
Ajay S <ajay@sicloot.com>
2022-04-22 21:43:15 -0400

    add css to nav breadcrumbs

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import {useContext} from 'react';
 import {DataContext} from '../Home';
 import { Route, Link, Redirect } from 'react-router-dom';
+import parse from 'html-react-parser';
 
 const Nav = () => {
   const dataContext = useContext(DataContext);
@@ -13,8 +14,8 @@ const Nav = () => {
     console.log('nav linkTaxon: ', linkTaxon);
     return (
       <>
-        { (index > 0) ? ' -> ' : '' }
-        <Link key={index} to={element.link} onClick={() => handleClick(index, linkTaxon)}>{element.name}</Link>
+        { (index > 0) ? parse('<li><span className="text-white mx-2">/</span></li>') : '' }
+        <li className="text-center"><Link key={index} to={element.link} className="text-blue-100 hover:text-blue-800" onClick={() => handleClick(index, linkTaxon)}>{element.name}</Link></li>
       </>
     );
   });
@@ -28,8 +29,21 @@ const Nav = () => {
     console.log('Nav handleClick taxonid set to ', taxonID);
   };
 
+  console.log('links is ', links);
+
+  //navbar css from tailwind-elements.com
   //return <div><Link to={`/${dataContext.location}/${dataContext.breadcrumbs[0].link}`}>{dataContext.breadcrumbs[0].name}</Link></div>;
-  return <div>{links}</div>;
+  return (
+    <nav className="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-400 text-xl text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <nav className="bg-grey-light rounded-md w-full" aria-label="breadcrumb">
+          <ol className="list-reset flex justify-center">
+            {links}
+          </ol>
+        </nav>
+      </div>
+    </nav>
+  );
 
 }
 
